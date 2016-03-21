82a27eef9a3b795132525ca062e60798ad7ef9d4
Maxim <maximyurinmusic@gmail.com>
2016-03-21 20:57:50 +0200

    Update script.js

--- JavaScript.Module_1-2/1/script.js
+++ JavaScript.Module_1-2/1/script.js
@@ -6,12 +6,24 @@ function pow(a, b) {
   var a = parseInt(baseNumber);
   var b = parseInt(exponentNumber);
   var result = a;
+  if (b === 0) {
+    result = a / a;
 
-   for(var j = 1; j < b; j++) {
+    return result;
+  }
+    else if (b < 0) {
+    result = 1 / Math.pow(a, b);
+
+    return 1 / result;
+  }
+    else {
+
+     for(var j = 1; j < b; j++) {
      result *= a;
-   }
+    }
 
-  return result;
+    return result;
+  }
 }
 
 console.log('RESULT' , exponentNumberResult);
