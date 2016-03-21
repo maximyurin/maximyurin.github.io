5eda27989781d858187686d4c1fb3f37622476bb
Maxim <maximyurinmusic@gmail.com>
2016-03-21 16:09:30 +0200

    Update script.js

--- JavaScript.Module_1-2/2/script.js
+++ JavaScript.Module_1-2/2/script.js
@@ -1,20 +1,20 @@
 var userNames = [];
 userNames.length = 5;
 
-for (i = 0; i<userNames.length; i++){
+  for (i = 0; i<userNames.length; i++) {
 	userNames[i] = prompt('Введите любое имя');
-}
+  }
 
 var userName = prompt('А теперь введите свое имя');
 
 console.log(userNames);
 console.log(userName);
 
-for (i = 0; i<userNames.length; i++){
-if(userNames[i] === userName){
-  var correct = (userName);
-}
-}
+  for (i = 0; i<userNames.length; i++) {
+   if(userNames[i] === userName) {
+   var correct = (userName);
+   }
+  }
 
 if(correct) {
 	alert('Поздравляем вы успешно вошли!');
