f26f09b388ff3af4d531172072c681c8bda60a08
Maxim <maximyurinmusic@gmail.com>
2016-05-20 23:11:58 +0300

    Create script.js

---
+++ JavaScript.Module_21-22/1/js/src/script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+
+
+
+var script={
+	myPow:function (num, power)
+	{
+
+			if (power === 0) return 1;
+			if (power % 2 === 1) return this.myPow(num, power - 1) * num;
+			else
+			{
+				var b = this.myPow(num, power / 2);
+				return b * b;
+		    }
+	}
+};
+
+
+	try {
+	module.exports=script;
+	} catch (e)
+	 {
+
+	 	var num = prompt("Enter Base Number");
+        var power = prompt("Enter Exponent Number");
+		var newResult = script.myPow(num, power);
+		console.log("The result is " + newResult);
+	}
+
+
+
+
+//# sourceMappingURL=script.js.map
